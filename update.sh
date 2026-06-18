10068007d28a462da5aee98305f24e090c9f5324
Tatsuya IKUTA <tatsuya.ikuta@gmail.com>
2026-06-18 13:57:31 +0900

    Update log

--- update.sh
+++ update.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-set -eu
-
 BASEDIR=$(dirname $(realpath $0))
-for d in /home/*/ /em/*data/ /em/analysis/*/; do echo -n "$d "; df -BT --output=used,avail,size,pcent "$d" | tail -1; done | column -t > $BASEDIR/quota.txt && \
-/opt/slurm/bin/squeue --json > $BASEDIR/squeue.json && \
-/opt/slurm/bin/sinfo -N -l --json > $BASEDIR/sinfo.json && \
-ssh argon leadm tape list > $BASEDIR/leadm.txt && \
-ssh argon  sudo /usr/Arcconf/arcconf getconfig 1 LD > $BASEDIR/arcconf.argon && \
-ssh neon  sudo /usr/Arcconf/arcconf getconfig 1 LD > $BASEDIR/arcconf.neon && \
-pmval -f 1  -s 1 -h bio2q003 nvidia.gpuactive > $BASEDIR/nvidia.gpuactive.bio2q003 && \
-pmval -f 1  -s 1 -h bio2q001 nvidia.gpuactive > $BASEDIR/nvidia.gpuactive.bio2q001 && \
-pmval -f 1  -s 1 -h bio2q003 kernel.all.cpu.user > $BASEDIR/kernel.all.cpu.user.bio2q003 && \
-pmval -f 1  -s 1 -h bio2q001 kernel.all.cpu.user > $BASEDIR/kernel.all.cpu.user.bio2q001 && \
-/usr/bin/python3 $BASEDIR/generate_dashboard.py && \
-cp $BASEDIR/dashboard.html /usr/share/caddy/index.html
 
-set +eu
+for d in /home/*/ /em/*data/ /em/analysis/*/; do
+    echo -n "$d "; df -BT --output=used,avail,size,pcent "$d" | tail -1
+done | column -t > $BASEDIR/quota.txt
+/opt/slurm/bin/squeue --json > $BASEDIR/squeue.json
+/opt/slurm/bin/sinfo -N -l --json > $BASEDIR/sinfo.json
+ssh argon leadm tape list > $BASEDIR/leadm.txt
+ssh argon  sudo /usr/Arcconf/arcconf getconfig 1 LD > $BASEDIR/arcconf.argon
+ssh neon  sudo /usr/Arcconf/arcconf getconfig 1 LD > $BASEDIR/arcconf.neon
+pmval -f 1  -s 1 -h bio2q003 nvidia.gpuactive > $BASEDIR/nvidia.gpuactive.bio2q003
+pmval -f 1  -s 1 -h bio2q001 nvidia.gpuactive > $BASEDIR/nvidia.gpuactive.bio2q001
+pmval -f 1  -s 1 -h bio2q003 kernel.all.cpu.user > $BASEDIR/kernel.all.cpu.user.bio2q003
+pmval -f 1  -s 1 -h bio2q001 kernel.all.cpu.user > $BASEDIR/kernel.all.cpu.user.bio2q001
+/usr/bin/python3 $BASEDIR/generate_dashboard.py
+cp $BASEDIR/dashboard.html /usr/share/caddy/index.html
